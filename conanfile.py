b486d5e43f2ef7af806d638ff475b3179584ea16
kilian <kcaillot@gmail.com>
2019-02-28 17:00:58 +0100

    version 2.20.6

--- conanfile.py
+++ conanfile.py
@@ -4,23 +4,23 @@ from conans import ConanFile, CMake, tools
 
 class Sol2Conan(ConanFile):
     name = "sol2"
-    version = "2.20.4"
+    version = "2.20.6"
     license = "MIT License"
     url = "https://github.com/AtaLuZiK/conan-sol2"
     description = "Sol is a C++ library binding to Lua."
     exports_sources = ["sol2-config.cmake"]
     options = {
-        "lua_version": ["5.3.4"],
+        "lua_version": ["5.3.3"],
         "single_header": [True, False],
     }
     default_options = (
-        "lua_version=5.3.4",
+        "lua_version=5.3.3",
         "single_header=False",
     )
     generators = "cmake"
 
     def requirements(self):
-        self.requires("lua/%s@zimmerk/stable" % self.options.lua_version)
+        self.requires("lua/%s@swissdotnet/testing" % self.options.lua_version)
 
     @property
     def zip_folder_name(self):
@@ -29,7 +29,7 @@ class Sol2Conan(ConanFile):
     def source(self):
         zip_name = "%s-cpp-%s.tar.gz" % (self.name, self.version)
         tools.download("https://github.com/ThePhD/sol2/archive/v%s.tar.gz" % self.version, zip_name)
-        # tools.check_md5(zip_name, "b12ab43ed0ea7618035421ccc330be25")
+        tools.check_md5(zip_name, "f515a1e7aa65087ebcfd3c343f4b3b34")
         tools.unzip(zip_name)
         os.unlink(zip_name)
         with tools.chdir(self.zip_folder_name):
@@ -37,6 +37,7 @@ class Sol2Conan(ConanFile):
                 '''project(sol2 VERSION 2.20.0 LANGUAGES CXX C)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
+
 
     def package(self):
         if self.options.single_header:
